e5b1df6f6552b16aa95cea3b767a834a788da2d3
paul <paul.primus@jku.at>
2026-05-16 21:34:35 +0200

    add beats; fix fp16-mixed error

--- src/dcase2026_task1/models/__init__.py
+++ src/dcase2026_task1/models/__init__.py
@@ -1,5 +1,4 @@
 from dcase2026_task1.models.audio_flamingo3 import AudioFlamingo3Model
-from dcase2026_task1.models.audioset_tagging import AudioSetTaggingModel
 from dcase2026_task1.models.base import (
     AudioTagScore,
     AudioTaggingInput,
@@ -14,7 +13,6 @@ from dcase2026_task1.models.qwen import QwenModel
 
 __all__ = [
     "AudioFlamingo3Model",
-    "AudioSetTaggingModel",
     "AudioTagScore",
     "AudioTaggingInput",
     "AudioTaggingModel",
